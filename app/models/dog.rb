5295f73d66adbc705115c23c1478d8ca570e7a0c
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-13 23:35:18 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+
+  @all = []
+
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    self.all << self
+  end
